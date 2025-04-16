70532b63e8688415d67546ec8d1d9c5fe3a208ac
Jan <janfrei01@gmail.com>
2025-04-16 16:29:20 +0200

    Initial commit

---
+++ src/main/java/ch/schachklubsg/swisschessproxy/SwisschessProxyApplication.java
@@ -0,0 +1,13 @@
+package ch.schachklubsg.swisschessproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwisschessProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwisschessProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/schachklubsg/swisschessproxy/SwisschessProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.schachklubsg.swisschessproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwisschessProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
